a818b9e512a98d9f58487ae1adc6e9eab8663c54
Hamzah Arrayhan <hamzahrayhan@gmail.com>
2021-09-15 22:46:20 +0700

    push lab 2

---
+++ 21cineplux/src/main/java/apap/tutorial/cineplux/service/BioskopService.java
@@ -0,0 +1,12 @@
+package apap.tutorial.cineplux.service;
+
+import apap.tutorial.cineplux.model.BioskopModel;
+import java.util.List;
+
+public interface BioskopService {
+
+    void addBioskop(BioskopModel bioskop);
+    List<BioskopModel> getBioskopList();
+    BioskopModel getBioskopByIdBioskop(String idBioskop);
+
+}
