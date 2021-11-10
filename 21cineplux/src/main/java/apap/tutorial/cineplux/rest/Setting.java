16f46f1b48d9f0f9926daf2d88611473785b2edf
Hamzah Arrayhan <hamzahrayhan@gmail.com>
2021-11-10 23:44:00 +0700

    feat/tutorial-5-21cineplux

---
+++ 21cineplux/src/main/java/apap/tutorial/cineplux/rest/Setting.java
@@ -0,0 +1,6 @@
+package apap.tutorial.cineplux.rest;
+
+public class Setting {
+    final public static String bioskopUrl = "https://fe111f30-013f-4606-a612-f544e3fb1e02.mock.pstmn.io";
+    final public static String umurUrl = "https://api.agify.io";
+}
